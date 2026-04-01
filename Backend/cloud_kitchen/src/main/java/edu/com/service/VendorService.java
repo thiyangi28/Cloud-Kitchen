5a6f8f7b421550157732719ea7d0e847d22529a9
thiyangi28 <thiyangip24@gmail.com>
2026-04-01 15:19:03 +0530

    feat:add service layer for controller classs  update and delete api

--- Backend/cloud_kitchen/src/main/java/edu/com/service/VendorService.java
+++ Backend/cloud_kitchen/src/main/java/edu/com/service/VendorService.java
@@ -7,4 +7,6 @@ public interface VendorService {
     String addVendor(VendorDto vendorDto);
     List<VendorDto> getAllVendors();
     VendorDto getVendorById(Long id);
+    boolean updateVendor(VendorDto vendorDto);
+    boolean deleteVendor(Long id);
 }
